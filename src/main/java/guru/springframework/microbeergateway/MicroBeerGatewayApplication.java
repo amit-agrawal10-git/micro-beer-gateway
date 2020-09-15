51ed45aeb18093434a482de91b577adf0bd41b90
amit-agrawal10 <amit.agrawal10@gmail.com>
2020-09-15 22:41:32 +0530

    Initial gateway project

---
+++ src/main/java/guru/springframework/microbeergateway/MicroBeerGatewayApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.microbeergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBeerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBeerGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/microbeergateway/MicroBeerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.microbeergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBeerGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
